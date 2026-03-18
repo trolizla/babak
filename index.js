92f18c4db98cdf165021e6c003d24c188f2beee1
baredmark583 <baredmark583@gmail.com>
2026-03-18 19:30:00 +0200

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+import { createLibp2p } from 'libp2p'
+import { websockets } from '@libp2p/websockets'
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { identify } from '@libp2p/identify'
+import { circuitRelayServer } from '@libp2p/circuit-relay-v2'
+
+async function main() {
+  const port = process.env.PORT || 10000;
+  
+  const node = await createLibp2p({
+    addresses: {
+      listen: [`/ip4/0.0.0.0/tcp/${port}/ws`]
+    },
+    transports: [websockets()],
+    connectionEncryption: [noise()],
+    streamMuxers: [yamux()],
+    services: {
+      identify: identify(),
+      relay: circuitRelayServer({
+        reservations: {
+          maxReservations: Infinity,
+          applyDefaultLimit: false
+        }
+      })
+    }
+  });
+
+  console.log(`Relay server WS listening on port ${port}...`);
+  node.getMultiaddrs().forEach((ma) => console.log(ma.toString()));
+}
+
+main();
